af97e5298b202910d79733ae71b062eb1d57d18b
davkrai <davkrai22@gmail.com>
2020-12-21 15:26:44 -0500

    SecciónÉxitos - David

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <section-exitos></section-exitos>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+
+import SectionExitos from "./components/SectionExitos.vue";
+
+export default {
+  components:{
+    SectionExitos        
+  }
+}
+</script>
 
 <style>
 #app {
