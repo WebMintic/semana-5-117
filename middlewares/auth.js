a6e4692ebe42a2c7746ff46c9aac65ec8aeb8895
Daniel <Df.Salazar0402@gmail.com>
2020-12-23 10:10:04 -0500

    retoques al backend

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@
 const tokenService = require('../services/token');
 
 module.exports = {
-    verifyUsuario: async(req, res, next) => {
+    verifyUsuarioAdmin: async(req, res, next) => {
         if (!req.headers.token) {
             return res.status(404).send({
                 message: 'No token'
@@ -10,12 +10,44 @@ module.exports = {
         }
         const response = await tokenService.decode(req.headers.token);
         
-        if (response.rol == 'Administrador' || response.rol == 'Vendedor' || response.rol == 'Almacenero') {
+        if (response.rol == 'Administrador') {
             next();
         } else {
             return res.status(403).send({
                 message: 'No autorizado'
             });
+        }
+    },
+    verifyUsuarioVendedor: async(req, res, next) => {
+        if (!req.headers.token) {
+            return res.status(404).send({
+                message: 'No token'
+            });
+        }
+        const response = await tokenService.decode(req.headers.token);
+        
+        if (response.rol == 'Vendedor') {
+            next();
+        } else {
+            return res.status(403).send({
+                message: 'No autorizado'
+            });
+        }
+    },
+    verifyUsuarioAlmacen: async(req, res, next) => {
+        if (!req.headers.token) {
+            return res.status(404).send({
+                message: 'No token'
+            });
+        }
+        const response = await tokenService.decode(req.headers.token);
+        
+        if (response.rol == 'Almacenero') {
+            next();
+        } else {
+            return res.status(403).send({
+                message: 'No autorizado'
+            }); 
         }
     },
 
